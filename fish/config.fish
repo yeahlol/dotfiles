60866368318abf603db96aba0629a84e1de5ad86
yeah lol <yeahlolllll@gmail.com>
2020-01-11 00:37:12 +0900

    update fish config

--- fish/config.fish
+++ fish/config.fish
@@ -1,12 +1,14 @@
-# Go
-set -x PATH $HOME/.goenv/bin:$PATH
-eval (goenv init - | source)
-
+# goenv
+set -x GOROOT (go env GOROOT)
 set -x GOPATH $HOME/.go
-set -x PATH $PATH:$GOPATH/bin
+set -x PATH $PATH $GOPATH/bin
+set -x PATH $HOME/.goenv/bin $PATH
+set -x GO111MODULE on
+set -x GOENV_SHELL fish
+eval (goenv init - | source)
 
 # alias
 alias dc='docker-compose'
 
 # direnv
-eval (direnv hook fish)
+eval (direnv hook fish | source)
